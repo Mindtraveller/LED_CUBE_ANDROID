6d91167dc4ac7f47d1a37a68b1ce90df906fcd41
Vladimir <vladimir.penyazkov@gmail.com>
2015-12-22 21:30:04 +0300

    init

---
+++ JSDroid/www/scripts/controller.js
@@ -0,0 +1,57 @@
+﻿var controller = {
+
+    showDevices : function() {
+        $("#joystick").hide();
+        $("#devices").show();
+
+        var getDeviceTemplate = function (device) {
+            var li = jQuery("<li></li>").append("<label>" + device.name + "</label><p>" + device.address + "</p>");
+            li.click(function() {
+                controller.connectTo(device.address);
+            });
+            return li;
+        };
+
+        bluetoothSerial.list(function(devices) {
+            devices.forEach(function(device) {
+                var deviceLabel = getDeviceTemplate(device);
+                $("#devices").append(deviceLabel);
+            });
+        });
+
+        //bluetoothSerial.discoverUnpaired(function (devices) {
+        //    devices.forEach(function(device) {
+        //        var deviceLabel = getDeviceTemplate(device);
+        //        $("#devices").append(deviceLabel);
+        //    });
+        //});
+
+        //bluetoothSerial.setDeviceDiscoveredListener(function (device) {
+        //        var deviceLabel = getDeviceTemplate(device);
+        //        $("#devices").append(deviceLabel);
+        //});
+    },
+
+    showJoystick: function () {
+        var joystick = $("#joystick");
+        joystick.removeClass("hidden");
+        joystick.show();
+    },
+
+    hideJoystick: function() {
+        var joystick = $("#joystick");
+        joystick.hide();
+    },
+
+    connectTo: function (address) {
+        bluetoothSerial.connect(address, function () {
+            $("#devices").hide();
+            $("#mode_buttons").removeClass("hidden");
+            $("#mode_buttons").show();
+        });
+    },
+
+    sendData: function (data) {
+        bluetoothSerial.write([data]);
+    }
+}
---
+++ JSDroid/www/scripts/index.js
@@ -0,0 +1,81 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        cordova.plugins.backgroundMode.enable();
+        var auto = false;
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        controller.showDevices();
+        var modeButtons = $("#mode_buttons");
+        modeButtons.find("#mode_0_button").click(function () {
+            auto = false;
+            controller.sendData(11);
+            controller.hideJoystick();
+        });
+        modeButtons.find("#mode_1_button").click(function () {
+            auto = false;
+            controller.sendData(12);
+            controller.hideJoystick();
+        });
+        modeButtons.find("#mode_snake_button").click(function () {
+            auto = false;
+            controller.sendData(10);
+            controller.showJoystick();
+        });
+        modeButtons.find("#mode_2_button").click(function () {
+            auto = false;
+            controller.sendData(14);
+            controller.hideJoystick();
+        });
+        modeButtons.find("#mode_3_button").click(function () {
+            auto = false;
+            controller.sendData(13);
+            controller.hideJoystick();
+            
+        });
+        modeButtons.find("#mode_auto_button").click(function () {
+            auto = true;
+            var currentMode = 11;
+            controller.hideJoystick();
+            var timeoutFunction = function () {
+                if (!auto) return;
+                controller.sendData(currentMode);
+                if (++currentMode === 14) {
+                    currentMode = 11;
+                }
+                setTimeout(timeoutFunction, 10000);
+            }
+            timeoutFunction();
+        });
+
+        var joystick = $("#joystick");
+        joystick.find("#left_button").click(function () {
+            controller.sendData(1);
+        });
+        joystick.find("#top_button").click(function () {
+            controller.sendData(2);
+        });
+        joystick.find("#right_button").click(function () {
+            controller.sendData(3);
+        });
+        joystick.find("#bottom_button").click(function () {
+            controller.sendData(4);
+        });
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
